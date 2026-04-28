6dc375cc45f45658ef1f7d7245085c44843b9298
Denys Bykanov <dbykanov@drbsystems.com>
2026-04-28 22:27:27 +0200

    fix: update PocketBase URL in environment configuration from localhost to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  pbUrl: '',
+  pbUrl: 'https://db.bydens.net',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  pbUrl: 'http://localhost:8090',
+  pbUrl: 'https://db.bydens.net',
 };
